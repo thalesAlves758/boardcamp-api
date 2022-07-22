a8650b7eccba5a8dddf209502bf0f96c3b95a1cc
Thales Alves <thalesalves758@gmail.com>
2022-07-21 23:22:47 -0300

    Create initial server files

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 4000; /* eslint-disable-line */
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
